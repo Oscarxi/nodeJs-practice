035ba671a4605c01a249b19b4be29d4face49d1f
Oscar <oscar@25sprout.com>
2023-04-05 17:01:01 +0800

    feat: static assets in public

--- express/src/app.js
+++ express/src/app.js
@@ -1,23 +1,24 @@
+const path = require('path');
 const express = require('express');
+
 const app = express();
-const port = 3000;
+
+const publicDirPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirPath));
 
 // route
-app.get('/', (req, res) => {
-    res.send("Welcome to Oscar's website!");
-});
+// app.get('/', (req, res) => {
+//     res.send("<h1>Welcome to Oscar's website!<h1>");
+// });
 
-app.get('/help', (req, res) => {
-    res.send("Help Page");
-});
+// app.get('/about', (req, res) => {
+//     res.send("About");
+// });
 
-app.get('/about', (req, res) => {
-    res.send("About");
-});
-
-app.get('/weather', (req, res) => {
-    res.send("Taipei");
-});
+// app.get('/weather', (req, res) => {
+//     res.send("Taipei");
+// });
 
 // run server
 app.listen(3000, () => {
