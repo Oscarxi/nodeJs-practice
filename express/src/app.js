cadac71c0b760fdc855e6fdea6860be1ef24d6be
Oscar <oscar@25sprout.com>
2023-04-05 18:41:46 +0800

    feat: use handlebars and update css

--- express/src/app.js
+++ express/src/app.js
@@ -1,24 +1,31 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
-const publicDirPath = path.join(__dirname, '../public');
+const publicPath = path.join(__dirname, '../public');
+const partialsPath = path.join(__dirname, '../views/partials');
 
-app.use(express.static(publicDirPath));
+app.set('view engine', 'hbs');
+hbs.registerPartials(partialsPath);
+
+app.use(express.static(publicPath));
 
 // route
-// app.get('/', (req, res) => {
-//     res.send("<h1>Welcome to Oscar's website!<h1>");
-// });
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
-// app.get('/about', (req, res) => {
-//     res.send("About");
-// });
+app.get('/about', (req, res) => {
+    res.render('about');
+});
 
-// app.get('/weather', (req, res) => {
-//     res.send("Taipei");
-// });
+app.get('*', (req, res) => {
+    res.render('404', {
+        errorMessage: 'Page Not Found'
+    });
+});
 
 // run server
 app.listen(3000, () => {
